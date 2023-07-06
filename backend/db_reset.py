d39150b814cd342b91b8c5d913d9ba7d3d0bcc21
Etvoila <etvoila.jw@gmail.com>
2023-07-07 00:39:47 +1200

    change db reset.py to set up initially with first competition

--- backend/db_reset.py
+++ backend/db_reset.py
@@ -1,9 +1,19 @@
-from server import db
+import datetime
+from dateutil.relativedelta import relativedelta
 
+from server import db
+from models import (
+    Competition,
+)
+
+TODAY = datetime.date.today()
 
 # reseting database
 db.drop_all()
 db.create_all()
 
+# start a new competition which ends in 1 month period starting from today.
+db.session.add(Competition(start_date=TODAY,
+               end_date=TODAY + relativedelta(months=1, seconds=-1)))
 
 db.session.commit()
